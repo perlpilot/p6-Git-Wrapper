198cda72a5e173dcdde26da8dd67b6b83dbdf4d2
Jonathan Scott Duff <duff@pobox.com>
2014-05-11 01:03:25 -0500

    Use the MOP to add methods

--- lib/Git/Wrapper.pm
+++ lib/Git/Wrapper.pm
@@ -35,25 +35,12 @@ class Git::Wrapper {
         return $log-parser.made.list;
     }
 
-    method init(*@p, *%n) { return self.run('init', |@p, |%n); }
-    method clone(*@p, *%n) { return self.run('clone', |@p, |%n); }
-    method branch(*@p, *%n) { return self.run('branch', |@p, |%n); }
-    method checkout(*@p,*%n) { return self.run('checkout',|@p, |%n); }
-    method add(*@p,*%n) { return self.run('add',|@p, |%n); }
-    method pull(*@p,*%n) { return self.run('pull',|@p, |%n); }
-    method reset(*@p,*%n) { return self.run('reset',|@p, |%n); }
-    method rebase(*@p,*%n) { return self.run('rebase',|@p, |%n); }
-    method push(*@p,*%n) { return self.run('push',|@p, |%n); }
-    method fetch(*@p,*%n) { return self.run('fetch',|@p, |%n); }
-    method commit(*@p,*%n) { return self.run('commit',|@p, |%n); }
-    method show(*@p,*%n) { return self.run('show',|@p, |%n); }
-    method status(*@p,*%n) { return self.run('status',|@p, |%n); }
-    method diff(*@p,*%n) { return self.run('diff',|@p, |%n); }
-    method grep(*@p,*%n) { return self.run('grep',|@p, |%n); }
-    method merge(*@p,*%n) { return self.run('merge',|@p, |%n); }
-    method mv(*@p,*%n) { return self.run('mv',|@p, |%n); }
-    method rm(*@p,*%n) { return self.run('rm',|@p, |%n); }
-    method tag(*@p,*%n) { return self.run('tag',|@p, |%n); }
+
+    for <init clone branch checkout add pull rebase reset push fetch commit show status diff grep merge mv rm tag> -> $method {
+        Git::Wrapper.HOW.add_method(Git::Wrapper, $method, anon method (*@p, *%n) {
+            return self.run($method, |@p, |%n); 
+        });
+    }
 
 }
 
